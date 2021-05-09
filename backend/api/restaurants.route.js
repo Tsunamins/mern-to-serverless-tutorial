06ffc034788862478a03da8a6e153cf710a9a2f3
Alison Reilly <reillyamr@gmail.com>
2021-05-09 12:55:40 -0400

    add code for controller, update route w controller

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,7 +1,8 @@
 import express from "express"
+import RestaurantsCtrl from "./restaurants.controller.js" //created next
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send("hello world"))
+router.route("/").get(RestaurantsCtrl.apiGetRestaurant) //created next
 
 export default router
