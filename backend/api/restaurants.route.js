3de9953c5281218608730f32f47075469f924d77
Alison Reilly <reillyamr@gmail.com>
2021-05-22 17:17:54 -0400

    add routes for reviews under restaurants

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,8 +1,15 @@
 import express from "express"
 import RestaurantsCtrl from "./restaurants.controller.js" //created next
+import ReviewsCtrl from "./reviews.controller.js"
 
 const router = express.Router()
 
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants) //created next
+
+router
+  .route("/review")
+  .post(ReviewsCtrl.apiPostReview)
+  .put(ReviewsCtrl.apiUpdateReview)
+  .delete(ReviewsCtrl.apiDeleteReview)
 
 export default router
