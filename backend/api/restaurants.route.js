89318883e22758f2c621aa822b78ce7f3826df0a
Alison Reilly <reillyamr@gmail.com>
2021-06-13 12:50:37 -0400

    added routes rest by id/w reviews, cuisines choice

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,10 +1,14 @@
 import express from "express"
-import RestaurantsCtrl from "./restaurants.controller.js" //created next
+import RestaurantsCtrl from "./restaurants.controller.js"
 import ReviewsCtrl from "./reviews.controller.js"
 
 const router = express.Router()
 
-router.route("/").get(RestaurantsCtrl.apiGetRestaurants) //created next
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+//route for getting rest by id and all assoc reviews
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById)
+//route for populating list of avail cuisines to choose from
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines)
 
 router
   .route("/review")
